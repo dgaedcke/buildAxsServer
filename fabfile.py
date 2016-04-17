f896fa5ca972a06fbeee0c9f3294f74cf56b057d
David Wittman <dave@wittman.xyz>
2016-04-17 17:07:04 -0500

    Add Vagrantfile

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,11 @@ from contextlib import contextmanager as _contextmanager
 
 import yaml
 
-from fabric.api import *
-from fabric.context_managers import settings
+from fabric.api import run, cd, env, lcd, sudo, put, local
+from fabric.context_managers import settings, prefix
+from fabric.contrib.files import exists
 from fabric.contrib.project import rsync_project
 from fabric.decorators import roles
-from fabric.api import run, cd, env, roles, lcd
-from fabric.contrib.files import exists
 
 env.activate = 'source /opt/paysys/python/bin/activate'
 app_user = 'paysys'
@@ -31,11 +30,27 @@ def loadenv(environment=None):
 @roles('application')
 def setup(wipe=False):
     """Sets up payment system"""
+    dependencies = (
+        "wget",
+        "build-essential",
+        "git",
+        "openssl-devel",
+        "zlib-devel",
+        "bzip2-devel",
+        "python-virtualenv",
+        "rabbitmq-server",
+        "mysql-server",
+        "npm"
+    )
+
     wipe = bool(wipe)
     if wipe or not exists('/opt/paysys'):
         sudo('rm -rf /opt/paysys')
+
     sudo('useradd -d /opt/paysys %s || true' % app_user)
-    sudo('yum install -q -y wget build-essential git openssl-devel zlib-devel python-virtualenv')
+    sudo('yum install -q -y %s' % ' '.join(dependencies))
+    sudo('npm install -g bower')
+
     if not exists('/opt/python2.7'):
         with cd('/tmp'):
             run('wget https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tgz')
@@ -58,41 +73,47 @@ def setup(wipe=False):
     put('./files/supervisord.conf', '/etc/supervisord.conf', use_sudo=True)
     sudo('service supervisord start')
     sudo('chkconfig supervisord on')
+    sudo('service rabbitmq-server start')
+    sudo('chkconfig rabbitmq-server on')
+    sudo('service mysqld start')
+    sudo('chkconfig mysqld on')
+    sudo('mysqladmin create pay')
+    sudo('mysql -e \'GRANT ALL ON pay.* TO `payApp`@localhost IDENTIFIED BY "apple1010"\'')
 
 @roles('application')
 def deploy(version='master'):
     """Deploys payment code to application server"""
     if not exists('/opt/paysys'):
         setup()
-    local('rm -rf paymentSystem')
-    local('/usr/bin/git clone git@github.com:dgaedcke/paymentSystem.git')
-    env.release = time.strftime('%Y%m%d%H%M%S')
-    with lcd('paymentSystem'):
-        local('git checkout ' + version)
-        local('git archive --format=tar ' + version + ' | gzip > /tmp/payment-' + env.release + '.tar.gz')
-    put('/tmp/payment-' + env.release + '.tar.gz', '/tmp/')
-    run('mkdir -p /opt/paysys/builds/' + env.release)
-    with cd('/opt/paysys/builds/' + env.release):
-        run('tar -zxf /tmp/payment-' + env.release + '.tar.gz')
-    run('rm /opt/paysys/current')
-    run('ln -sf /opt/paysys/builds/' + env.release + ' /opt/paysys/current')
-    put(config_dir + '/files/' + env.config['environment'] + '/local_settings.py', '/opt/paysys/current/source/configuration/local_settings.py')
-    with cd('/opt/paysys/current'):
-        with virtualenv():
-            run('pip install -r requirements/common.txt')
-    with cd('/opt/paysys/current/source'):
-        with virtualenv():
-            run('pip install --editable .')
-    put(config_dir + '/files/' + env.config['environment'] + '/supervisor-payment.conf', '/etc/supervisor.d/payment.conf', use_sudo=True)
-    with cd('/opt/paysys/current/source/web/static'):
-        run('rm -rf ~/.cache')
-        run('bower install --config.interactive=false -s')
-    if not exists('/opt/paysys/rules/__init__.py'):
-        sudo('/bin/cp -r /opt/paysys/current/rules-initial/* /opt/paysys/rules/')
-        sudo('chown -R ' + app_user + ':' + app_user + ' /opt/paysys/rules')
-    run('rm -rf /opt/paysys/current/source/rules')
-    run('ln -sf /opt/paysys/rules /opt/paysys/current/source/rules')
-    sudo('/opt/paysys/python/bin/supervisorctl reread')
+
+    sudo('mkdir /opt/paysys/.ssh; chmod 0700 /opt/paysys/.ssh')
+    put('./files/id_deploy', '/opt/paysys/.ssh/id_rsa', mode=0400, use_sudo=True)
+    sudo('chown -R %s. /opt/paysys/.ssh' % app_user)
+
+    with settings(sudo_user=app_user), cd('/opt/paysys'):
+        # TODO(dw): rm -rf first?
+        sudo('ssh-keyscan github.com >> ~/.ssh/known_hosts')
+        sudo('git clone git@github.com:dgaedcke/paymentSystem.git /opt/paysys/current')
+        with cd('/opt/paysys/current'):
+            with virtualenv():
+                sudo('pip install -r requirements/common.txt')
+        with cd('/opt/paysys/current/source'):
+            with virtualenv():
+                sudo('pip install --editable .')
+        if not exists('/opt/paysys/rules/__init__.py'):
+            sudo('/bin/cp -r /opt/paysys/current/rules-initial/* /opt/paysys/rules/')
+        sudo('ln -sf /opt/paysys/rules /opt/paysys/current/source/rules')
+
+    put('./files/supervisor-payment.conf', '/etc/supervisor.d/payment.conf', use_sudo=True)
+    sudo('rm -rf /opt/paysys/current/source/web/static/.cache')
+
+    with settings(sudo_user=app_user), cd('/opt/paysys/current/source/web/static'):
+        sudo('bower install --config.interactive=false -s')
+
+    put('./files/local_settings.py', '/opt/paysys/current/source/configuration/local_settings.py', use_sudo=True)
+
+    sudo('rm -rf /opt/paysys/current/source/rules')
+    sudo('service supervisord restart')
     sudo('/opt/paysys/python/bin/supervisorctl restart payment')
     sudo('/opt/paysys/python/bin/supervisorctl restart celery')
     sudo('/opt/paysys/python/bin/supervisorctl restart celerybeat')
---
+++ files/local_settings.py
@@ -0,0 +1,58 @@
+# DB settings
+HOST = '127.0.0.1'
+PORT = '3306'
+USER = 'payApp'
+PW = 'apple1010'
+DB_NAME = 'pay'
+DB_ECHO = False
+
+# FILE system.
+PATH_PREFIX = '/opt/paysys/'
+PATH_WWW_DIR = PATH_PREFIX + 'www/'
+PATH_SOURCE_DIR = PATH_PREFIX + 'paySystem/source/'
+PATH_JOB_DIR = PATH_SOURCE_DIR + 'jobs/'
+PATH_LOG_DIR = PATH_PREFIX + 'logs/'
+PATH_FTP_DIR = PATH_PREFIX + 'ftp/'
+PATH_UPLOAD_DIR = PATH_SOURCE_DIR + 'web/uploads/'
+PATH_RULE_GEN_DIR = PATH_PREFIX + 'rules/'
+
+# CELERY.
+TIMEZONE = 'America/Denver'
+SEND_TASK_ERROR_EMAILS = True
+SKIP_ASYNC_INFRASTRUCTURE = False
+
+# Top email is the default for job notification if no email is specified.
+ADMINS = [
+    ('Dewey Gaedcke', 'dewey@pathoz.com'),
+    ('Andy Lasada', 'Alasda@examiner.com'),
+    ('Marc Ingram', 'mIngram@examiner.com')
+]
+
+# EMAIL.
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USER = 'dewey@minggl.com'
+EMAIL_PW = 'LUARIBBIT'
+EMAIL_PORT = '465'
+EMAIL_USE_SSL = True
+
+MAIL_CONFIG = {'host': EMAIL_HOST, 'un': EMAIL_USER, 'pw': EMAIL_PW, 'port': EMAIL_PORT, 'use_ssl': EMAIL_USE_SSL}
+
+# FLASK / WEB.
+WEB_HOST_DOMAIN = 'localhost'
+WEB_HOST_PORT = ':5000'
+
+# Used to generate the download URL for files in job-notification emails.
+_host_template = 'http://%s%s'
+WEB_HOST = _host_template % (WEB_HOST_DOMAIN, WEB_HOST_PORT)
+
+_file_dl_template = _host_template + '/files/'
+DL_URL_PREFIX = _file_dl_template % (WEB_HOST_DOMAIN, WEB_HOST_PORT)
+
+FLASK_DEBUG = True
+WEB_SECRET_KEY = 'broohaha123'
+
+# RABBIT-MQ.
+BROKER_URL = 'amqp://'
+
+# JOB SERVER.
+RENAME_COMPLETED_FILES = True
